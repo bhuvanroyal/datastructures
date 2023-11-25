b4436727012b8b84ea8d5eaa5841347ab5416e14
bhuvan <damabhuvan@gmail.com>
2023-11-25 12:12:11 +0530

    subsequence problem solved

--- src/recursion/Subsequence.java
+++ src/recursion/Subsequence.java
@@ -5,19 +5,31 @@ import java.util.List;
 
 public class Subsequence {
 	
-	public static void subsequence(int i,int[] arr,ArrayList<Integer[]> lists) {
+	public static void subsequence(int i,int[] arr,ArrayList<ArrayList<Integer>> lists,ArrayList<Integer> list) {
 		
-		if(i>arr.length) {
+		if(i==arr.length) {
+//			System.out.println(list);
+			lists.add(new ArrayList<>(list));
 			return;
+			
 		}
 		
+		list.add(arr[i]);
+		subsequence(i+1,arr,lists,list);
+		
+		list.remove(list.size()-1);
+		subsequence(i+1,arr,lists,list);
 		
 	}
 
 	public static void main(String[] args) {
-		ArrayList<Integer[]> lists=new ArrayList<>();
-		int[] arr= {1,2,3};
-		subsequence(0,arr,lists);
+		ArrayList<Integer> list=new ArrayList<>();
+		ArrayList<ArrayList<Integer>> lists=new ArrayList<>();
+		int[] arr= {1,2,3,4};
+		subsequence(0,arr,lists,list);
+		for (ArrayList<Integer> subsequence : lists) {
+	        System.out.println(subsequence);
+	    }
 
 	}
 
--- src/recursion/SubsequencesOfString.java
+++ src/recursion/SubsequencesOfString.java
@@ -5,6 +5,20 @@ public class SubsequencesOfString{
 	public static void subsequence(String str) {
 		
 		Queue<String> queue=new LinkedList<>();
+		for(int i=0;i<str.length();i++) {
+			queue.add(String.valueOf(i));
+			while(!queue.isEmpty()) {
+				String temp=queue.remove();
+				String sum="";
+				for(String k:temp.split("")) {
+					sum+=String.valueOf(str.charAt(Integer.parseInt(k)));
+				}
+				System.out.println(sum);
+				for(int j=Integer.valueOf(temp.charAt(temp.length()-1));j<str.length();j++) {
+					queue.add(temp+String.valueOf(j));
+				}
+			}
+		}
 		
 	}
 	
